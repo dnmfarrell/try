88ed53aa615416de0e4281442025bb88615b38c9
David Farrell <1469333+dnmfarrell@users.noreply.github.com>
2023-02-22 22:52:27 -0500

    Compact to diamond-like perfection

--- try.go
+++ try.go
@@ -1,48 +1,15 @@
+// Package try provides an either monad for bundling return vales and
+// exceptions. It includes standard functions like bind, fmap and return.
 package try
 
+// Try is a monadic either type which contains a generic value or an error.
 type Try[A any] struct {
 	Err error
 	Val A
 }
 
-func Fmap[A, B any](a Try[A], f func(a A) Try[B]) Try[B] {
-	if a.Err == nil {
-		return f(a.Val)
-	}
-	return Fail[B](a.Err)
-}
-
-func Fmap2[A, B, C any](a Try[A], b Try[B], f func(a A, b B) Try[C]) Try[C] {
-	if a.Err == nil {
-		if b.Err == nil {
-			return f(a.Val, b.Val)
-		}
-		return Fail[C](b.Err)
-	}
-	return Fail[C](a.Err)
-}
-
-func Compose[A, B, C any](f func(a A) Try[B], g func(b B) Try[C]) func(A) Try[C] {
-	return func(a A) Try[C] { return Fmap(f(a), g) }
-}
-
-func Map[A, B any](a Try[A], f func(A) B) Try[B] {
-	if a.Err == nil {
-		return Succeed[B](f(a.Val))
-	}
-	return Fail[B](a.Err)
-}
-
-func Map2[A, B, C any](a Try[A], b Try[B], f func(A, B) C) Try[C] {
-	if a.Err == nil {
-		if b.Err == nil {
-			return Succeed[C](f(a.Val, b.Val))
-		}
-		return Fail[C](b.Err)
-	}
-	return Fail[C](a.Err)
-}
-
+// New converts a function that returns a value and an error into one that
+// returns a Try.
 func New[A, B any](f func(a A) (B, error)) func(A) Try[B] {
 	return func(a A) Try[B] {
 		val, err := f(a)
@@ -53,24 +20,31 @@ func New[A, B any](f func(a A) (B, error)) func(A) Try[B] {
 	}
 }
 
-func New2[A, B, C any](f func(a A, b B) (C, error)) func(A, B) Try[C] {
-	return func(a A, b B) Try[C] {
-		val, err := f(a, b)
-		if err == nil {
-			return Succeed[C](val)
-		}
-		return Fail[C](err)
-	}
-}
-
-func Lift[A, B any](f func(A) B) func(Try[A]) Try[B] {
-	return func(a Try[A]) Try[B] { return Map(a, f) }
-}
-
-func Lift2[A, B, C any](f func(A, B) C) func(Try[A], Try[B]) Try[C] {
-	return func(a Try[A], b Try[B]) Try[C] { return Map2(a, b, f) }
-}
-
+// Fail constructs a Try that wraps an error (aka "return").
 func Fail[A any](err error) Try[A] { return Try[A]{Err: err} }
 
+// Succeed constructs a Try that wraps a value (aka "return").
 func Succeed[A any](val A) Try[A] { return Try[A]{Val: val} }
+
+// Fmap applies a Try to a non-monadic function, wrapping the return value as a
+// Try. The function is only called when the Try is not an error.
+func Fmap[A, B any](a Try[A], f func(A) B) Try[B] {
+	if a.Err == nil {
+		return Succeed[B](f(a.Val))
+	}
+	return Fail[B](a.Err)
+}
+
+// Bind applies a Try to a function which accepts a value and returns a Try.
+// The function is only called if the Try is not an error.
+func Bind[A, B any](a Try[A], f func(A) Try[B]) Try[B] {
+	if a.Err == nil {
+		return f(a.Val)
+	}
+	return Fail[B](a.Err)
+}
+
+// Compose takes two functions and combines them into a new function.
+func Compose[A, B, C any](f func(A) Try[B], g func(B) Try[C]) func(A) Try[C] {
+	return func(a A) Try[C] { return Bind(f(a), g) }
+}
--- try_test.go
+++ try_test.go
@@ -1,54 +1,27 @@
 package try
 
 import (
-	"errors"
 	"testing"
 )
 
 func TestMonadLaws(t *testing.T) {
-	// Compose(f, unit) == f
-	a := 5
+	// Left identity: "return bind is neutral".
+	// return x >>= f eq f x
+	x := 5
 	f := func(i int) Try[int] { return Succeed[int](i * 2) }
-	if Compose[int, int, int](f, Succeed[int])(a).Val != f(a).Val {
-		t.Errorf("Success does not obey left identity law")
+	if Bind[int](Succeed[int](x), f).Val != f(x).Val {
+		t.Errorf("Does not obey left identity law")
 	}
-	// Compose(unit, f) == f
-	if Compose[int, int, int](Succeed[int], f)(a).Val != f(a).Val {
-		t.Errorf("Success does not obey right identity law")
+	// Right identity: "bind return is neutral".
+	// m >>= return eq m
+	m := Succeed[int](x)
+	if Bind[int](m, Succeed[int]).Val != m.Val {
+		t.Errorf("Does not obey right identity law")
 	}
-	// bind(bind(m,f), g) == bind(m, fg)
-	m := Succeed[int](a)
+	// Associativity: "bind order does not matter".
+	// (m >>= f) >>= g eq m >>= (\x -> f x >>= g)
 	g := func(i int) Try[int] { return Succeed[int](i / 2) }
-	if Fmap(Fmap(m, f), g).Val != Fmap(m, Compose[int, int, int](f, g)).Val {
-		t.Error("Success does not obey associativity law")
-	}
-	M := Fail[int](errors.New("womp womp"))
-	if Fmap(Fmap(M, f), g).Err != Fmap(M, Compose[int, int, int](f, g)).Err {
-		t.Error("Failure does not obey associativity law")
-	}
-}
-
-func TestLift(t *testing.T) {
-	f := func(i int) int { return i + 2 }
-	g := Lift(f)
-	if g(Succeed[int](5)).Val != 7 {
-		t.Error("Map doesn't call lifted function on success")
-	}
-	if g(Fail[int](errors.New("ruhroh"))).Val == 2 {
-		t.Error("Map calls lifted function on failure")
-	}
-}
-
-func TestLift2(t *testing.T) {
-	f := func(x int, y int) int { return x + y }
-	g := Lift2(f)
-	if g(Succeed[int](5), Succeed[int](4)).Val != 9 {
-		t.Error("Map2 doesn't call lifted function on success")
-	}
-	if g(Fail[int](errors.New("ruhroh")), Succeed[int](4)).Val == 4 {
-		t.Error("Map2 calls lifted function on Fail[A]")
-	}
-	if g(Succeed[int](4), Fail[int](errors.New("ruhroh"))).Val == 4 {
-		t.Error("Map2 calls lifted function on Fail[B]")
+	if Bind[int](Bind[int](m, f), g).Val != Bind[int](f(x), g).Val {
+		t.Error("Does not obey associativity law")
 	}
 }
