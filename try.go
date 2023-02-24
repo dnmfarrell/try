b511bff8f809894e10e4d64e5943e0d30e83fba6
David Farrell <1469333+dnmfarrell@users.noreply.github.com>
2023-02-24 09:07:18 -0500

    Test every branch

--- try.go
+++ try.go
@@ -43,8 +43,3 @@ func Bind[A, B any](a Try[A], f func(A) Try[B]) Try[B] {
 	}
 	return Fail[B](a.Err)
 }
-
-// Compose takes two functions and combines them into a new function.
-func Compose[A, B, C any](f func(A) Try[B], g func(B) Try[C]) func(A) Try[C] {
-	return func(a A) Try[C] { return Bind(f(a), g) }
-}
--- try_test.go
+++ try_test.go
@@ -1,6 +1,8 @@
 package try
 
 import (
+	"errors"
+	"strconv"
 	"testing"
 )
 
@@ -8,20 +10,54 @@ func TestMonadLaws(t *testing.T) {
 	// Left identity: "return bind is neutral".
 	// return x >>= f eq f x
 	x := 5
-	f := func(i int) Try[int] { return Succeed[int](i * 2) }
-	if Bind[int](Succeed[int](x), f).Val != f(x).Val {
+	f := func(i int) Try[int] { return Succeed(i * 2) }
+	if Bind(Succeed(x), f).Val != f(x).Val {
 		t.Errorf("Does not obey left identity law")
 	}
 	// Right identity: "bind return is neutral".
 	// m >>= return eq m
-	m := Succeed[int](x)
-	if Bind[int](m, Succeed[int]).Val != m.Val {
+	m := Succeed(x)
+	if Bind(m, Succeed[int]).Val != m.Val {
 		t.Errorf("Does not obey right identity law")
 	}
 	// Associativity: "bind order does not matter".
 	// (m >>= f) >>= g eq m >>= (\x -> f x >>= g)
-	g := func(i int) Try[int] { return Succeed[int](i / 2) }
-	if Bind[int](Bind[int](m, f), g).Val != Bind[int](f(x), g).Val {
+	g := func(i int) Try[int] { return Succeed(i / 2) }
+	if Bind(Bind(m, f), g).Val != Bind(f(x), g).Val {
 		t.Error("Does not obey associativity law")
+	}
+}
+
+func TestNew(t *testing.T) {
+	tryParseBool := New(strconv.ParseBool)
+	if !tryParseBool("true").Val {
+		t.Error("Monadic parseBool didn't return parse value")
+	}
+	if tryParseBool("foo").Err == nil {
+		t.Error("Monadic parseBool didn't return parse error")
+	}
+}
+
+func TestFmap(t *testing.T) {
+	i := func(s string) string { return s }
+	s := Succeed[string]("x")
+	if Fmap(s, i).Val != "x" {
+		t.Error("Fmap didn't apply monad")
+	}
+	e := Fail[string](errors.New(""))
+	if Fmap(e, i).Err == nil {
+		t.Error("Fmap didn't return an error")
+	}
+}
+
+func TestBind(t *testing.T) {
+	i := func(s string) Try[string] { return Succeed(s) }
+	s := Succeed[string]("x")
+	if Bind(s, i).Val != "x" {
+		t.Error("Bind didn't apply monad")
+	}
+	e := Fail[string](errors.New(""))
+	if Bind(e, i).Err == nil {
+		t.Error("Bind didn't return an error")
 	}
 }
